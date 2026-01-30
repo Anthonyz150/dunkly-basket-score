2c21a98ae4a00c4ac29ea3c849b4912674295430
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-01-30 23:34:39 +0100

    Random

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+// app/loading.tsx
+export default function Loading() {
+    return (
+      <div className="loader-container">
+        <div className="bouncing-ball">🏀</div>
+        <p>Chargement de DUNKLY...</p>
+      </div>
+    );
+  }
